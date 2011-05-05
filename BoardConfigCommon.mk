62366a3116ef9c88f72ee79023f0e550457cb048
Brint E. Kriebel <github@bekit.net>
2011-05-05 10:05:05 -0700

    smb-common: Update options for proper recovery
    Remove kernel command line
        different devices have different partition layouts and this caused
        issues when they didn't match up
    
    Remove deprecated BOARD_HAS_NO_MISC_PARTITION
    
    Rename init.harmony.rc to init.nvidia.rc
        This gives proper naming based on /proc/cpuinfo since it is no longer
        specified in the kernel command line
    
    Add init_recovery.rc for proper ROM Manager detection

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -35,7 +35,8 @@ WIFI_DRIVER_MODULE_NAME     := "dhd"
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_CSR := true
 
-BOARD_KERNEL_CMDLINE := mem=448M@0M nvmem=64M@448M vmalloc=192M video=tegrafb console=ttyS0,115200n8 usbcore.old_scheme_first=1 cpuid=200102 devicetype=1002 btmac=9c5ed6131a00 tegraboot=nand mtdparts=tegra_nand:16384K@12032K(misc),16384K@62208K(recovery),16384K@79104K(boot),204800K@96000K(system),222464K@301312K(cache),4096K@7424K(bootbmp),32768K@28928K(logodata) androidboot.hardware=harmony
+BOARD_KERNEL_CMDLINE :=
+
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_PAGE_SIZE := 0x00000800
 
@@ -67,9 +68,6 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01000000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0c800000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_FLASH_BLOCK_SIZE := 131072
-
-# Setting this to avoid boot locks on the system from using the "misc" partition.
-BOARD_HAS_NO_MISC_PARTITION := true
 
 # Indicate that the board has an Internal SD Card
 BOARD_HAS_SDCARD_INTERNAL := true
